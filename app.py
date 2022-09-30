c396b2decef1f519e847fe78046a04c4da0e0d71
Konstantine Pialov <konstantinepialov@Konstantines-MacBook-Air.local>
2022-09-30 16:34:56 +0600

    Started preparing Django as backend

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from aiogram import executor
 from handlers import dp
+import os
+import django
 
 
-if __name__ == '__main__':
-    from aiogram import executor
-    from handlers import dp
+def setup_django():
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "bot_backend.bot_backend.settings"
+    )
+
+    os.environ.update(
+        {'DJANGO_ALLOW_ASYNC_UNSAFE': "true"}
+    )
+
+    django.setup()
+
+
+if __name__ == '__main__':    
+    setup_django()
 
     executor.start_polling(dp, skip_updates=True)
